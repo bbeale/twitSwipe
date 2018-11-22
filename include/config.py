539c8287ba3e714214313682bfcc23fcb09e591f
Ben <beale.ben@gmail.com>
2018-11-22 10:01:59 -0500

    Delete config.py

--- include/config.py
+++
@@ -1,6 +0,0 @@
-
-img_key             = "monkey"
-consumer_key        = "noRqlB0ytOvwiVXcFJsCe7ivc"
-consumer_secret     = "V8IbLXAylWJFHJWLP0YcM8gKkbI2C8xFBGQbU0BVCLIkaWVbSS"
-access_token        = "1639217659-DciiM1F0v6H6EXjQa46jRWQOlwrufMF3gKyBMra"
-access_token_secret = "NGdkqzBXSMknp0ZRO55XCLUfJunOvnfhY4oqioxj7ixit"
